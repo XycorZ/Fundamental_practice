95b4cf3db516ead4225793d0a6a050400001ef8a
XycorZ <XycorZ7@outlook.com>
2025-09-25 00:31:33 +0100

    Update voting.c

--- voting.c
+++ voting.c
@@ -5,6 +5,6 @@ int main(void){
     int voter_age ;
     printf("What is your age: \n");
     scanf("%d", &voter_age);
-    (voter_age <= voting_age) ? printf("Your too young") : printf("Welcome to vote");
+    (voter_age < voting_age) ? printf("Your too young") : printf("Welcome to vote");
     return 0;
 }
