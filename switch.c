689064ff3c8f1514cd4ee05cc50dbacfc01bdc93
XycorZ <XycorZ7@outlook.com>
2025-09-25 00:45:20 +0100

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+    int caseO = 1;
+    switch (caseO){
+        case 1:
+            printf("Test 1");
+            break;
+        case 2:
+            printf("Test 2");
+            break;
+        case 3:
+            printf("Test 3");
+            break;
+    }
+    return 0;
+}
