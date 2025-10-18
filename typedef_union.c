5efd612bd208581ac71ff7d264f4c9b2d5bf8cf7
XycorZ <XycorZ7@outlook.com>
2025-10-18 02:07:45 +0100

    Create typedef_union.c

---
+++ typedef_union.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef union{
+    int age;
+    char gender;
+    char animal[50];
+} man;
+
+int main(void){
+    man Tom;
+    Tom.age = 26;
+    Tom.gender = 'M';
+    strcpy(Tom.animal,"Cat");
+
+    printf("%d %c %s",Tom.age, Tom.gender, Tom.animal);
+    return -1;
+};
